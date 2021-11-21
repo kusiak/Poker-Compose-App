a165af59c62493633f74376e119bc66679b3c2fe
Adam Kusiak <ad.kusiak@gmail.com>
2021-11-21 16:08:45 +0100

    Initial changes

---
+++ app/src/androidTest/java/pl/pracownia/pokertables/MainInstrumentedTest.kt
@@ -0,0 +1,25 @@
+package pl.pracownia.pokertables
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+@RunWith(AndroidJUnit4::class)
+class MainInstrumentedTest {
+
+  @get:Rule
+  val composeTestRule = createComposeRule()
+
+  @Test
+  fun useAppContext() {
+    // Context of the app under test.
+    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    assertEquals("pl.pracownia.pokertables", appContext.packageName)
+  }
+}
